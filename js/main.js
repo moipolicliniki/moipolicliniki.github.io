aa8b867eab07e72998e1513d8594b9bb1dda10b7
moipolicliniki <51268287+moipolicliniki@users.noreply.github.com>
2019-06-18 15:08:52 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function getAndroidVersion(ua) {
+    ua = (ua || navigator.userAgent).toLowerCase(); 
+    var match = ua.match(/android\s([0-9\.]*)/);
+    return match ? match[1] : undefined;
+};
+	
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    if (/android/i.test(userAgent)) {
+         var check2= parseInt(getAndroidVersion(), 10);
+	    if(check2<6){
+		    return "bad";
+	       };
+       return "ок";
+    }
+    return "unknown";
+}
+var check=getMobileOperatingSystem();
+if(check=='bad'){
+   window.location.href = "http://arssent.github.io/test/error.html";
+   };
+   
+   
